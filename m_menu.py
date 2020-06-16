884e81b97a0b5d579fd2ccf7547cd7eb6931ff81
unknown <felibarreto2013@gmail.com>
2020-06-16 10:49:43 -0300

    Se crea un menú en PySimpleGUI que es distinto si hay una partida guardada o no y se retorna la opción elegida

---
+++ m_menu.py
@@ -0,0 +1,23 @@
+import PySimpleGUI as sg
+
+def menu():
+    hay_partida_guardada = True
+    
+    try:
+        archivo_save = open('savewindow.pickle', 'rb')
+        archivo_save.close()
+        archivo_save = open('datos_usuario.pickle', 'rb')
+        archivo_save.close()
+    except EOFError: ## si no hay save, no tendria porque haber problema
+        hay_partida_guardada = False
+    except FileNotFoundError:
+        hay_partida_guardada = False
+            
+    if hay_partida_guardada:
+        layout = [[sg.Button('Nueva partida',size =(10,5)), sg.Button('Cargar partida',size =(10,5)), sg.Button('Configurar',size =(10,5)), sg.Button('Top ten\npuntajes',size =(10,5))]]
+    else:
+        layout = [[sg.Button('Nueva partida',size =(10,5)), sg.Button('Configurar',size =(10,5)), sg.Button('Top ten\npuntajes',size =(10,5))]]
+    window = sg.Window('Menú principal',layout)
+    event, values = window.read()
+    window.close()
+    return event
