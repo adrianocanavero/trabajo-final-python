cd9d7532f0fb2fa8b77bb57e5855d15ebd527d66
unknown <felibarreto2013@gmail.com>
2020-07-05 12:15:05 -0300

    Las funciones crear_boton y calcular_puntos ahora reciben el parámetro nivel (y se lo envían a las funciones de los casilleros especiales de m_fichas) porque necesitan saber que nivel es para saber donde estan los casilleros especiales

--- m_tablero.py
+++ m_tablero.py
@@ -5,27 +5,27 @@ import m_fichas
 posicion_jugador = 0 # Es la posición en la que hay que poner la próxima palabra y puntaje de una jugada en concreta
 posicion_maquina = 0 # Es la posición en la que hay que poner la próxima palabra y puntaje de una jugada en concreta
 
-def crear_boton(i,j,AN,AL):
+def crear_boton(i,j,AN,AL,nivel):
 
     """Dependiendo del valor de (i,j) en la iteración que crea el tablero, 
         devuelve el boton específico de esas coordenadas"""
 
-    if (m_fichas.triplicar_palabra(i,j)):
-        return sg.Button("TPPP", size=(AN, AL), key=(i,j), pad=(0,0))
-    if (m_fichas.descuento_tres(i,j)):
-        return sg.Button("D3P", size=(AN, AL), key=(i,j), pad=(0,0))
-    if (m_fichas.triplicar_letra(i,j)):
-        return sg.Button("TPPL", size=(AN, AL), key=(i,j), pad=(0,0))
-    if (m_fichas.descuento_uno(i,j)):
-        return sg.Button("D1P", size=(AN, AL), key=(i,j), pad=(0,0))    
-    if (m_fichas.duplicar_palabra(i,j)):
-        return sg.Button("DPPP", size=(AN, AL), key=(i,j), pad=(0,0))
-    if (m_fichas.descuento_dos(i,j)):
-        return sg.Button("D2P", size=(AN, AL), key=(i,j), pad=(0,0))
-    if (m_fichas.duplicar_letra(i,j)):
-        return sg.Button("DPPL", size=(AN, AL), key=(i,j), pad=(0,0))
+    if (m_fichas.triplicar_palabra(i,j,nivel)):
+        return sg.Button("TPPP", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', 'dark red'))
+    if (m_fichas.descuento_tres(i,j,nivel)):
+        return sg.Button("D3P", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', 'indigo'))
+    if (m_fichas.triplicar_letra(i,j,nivel)):
+        return sg.Button("TPPL", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', '#533407'))
+    if (m_fichas.descuento_uno(i,j,nivel)):
+        return sg.Button("D1P", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', 'dark blue'))    
+    if (m_fichas.duplicar_palabra(i,j,nivel)):
+        return sg.Button("DPPP", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', '#08927d'))
+    if (m_fichas.descuento_dos(i,j,nivel)):
+        return sg.Button("D2P", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', 'dark orange'))
+    if (m_fichas.duplicar_letra(i,j,nivel)):
+        return sg.Button("DPPL", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', 'dark green'))
     if (m_fichas.es_inicio(i,j)):
-        return sg.Button("INICIO", size=(AN, AL), key=(i,j), pad=(0,0))
+        return sg.Button("INICIO", size=(AN, AL), key=(i,j), pad=(0,0), button_color=('white', 'grey'))
     return sg.Button("?", size=(AN, AL), key=(i,j), pad=(0,0))
 
 def actualizar_puntos(a_quien_actualizar,window,puntos):
@@ -37,7 +37,7 @@ def actualizar_puntos(a_quien_actualizar,window,puntos):
     else:
         window[(888,1)].update("PUNTOS\n"+str(puntos))
         
-def calcular_puntos(palabra,lugares_usados,valores_letras):
+def calcular_puntos(palabra,lugares_usados,valores_letras,nivel):
 
     """Calcula el valor final de la palabra segun los lugares en los que se encuentran
         sus letras y el puntaje de cada letra"""
@@ -48,19 +48,19 @@ def calcular_puntos(palabra,lugares_usados,valores_letras):
     triplicar_la_palabra = False # Hay que asegurarse que solo se pueda caer en un triplicar puntos por palabra a la vez
     duplicar_la_palabra = False # Hay que asegurarse que solo se pueda caer en un duplicar puntos por palabra a la vez
     for letra in palabra:
-        if (m_fichas.triplicar_palabra(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        if (m_fichas.triplicar_palabra(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             triplicar_la_palabra = True
-        elif (m_fichas.duplicar_palabra(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        elif (m_fichas.duplicar_palabra(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             duplicar_la_palabra = True
-        if (m_fichas.triplicar_letra(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        if (m_fichas.triplicar_letra(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             puntos += valores_letras[letra]*3
-        elif (m_fichas.duplicar_letra(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        elif (m_fichas.duplicar_letra(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             puntos += valores_letras[letra]*2
-        elif (m_fichas.descuento_tres(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        elif (m_fichas.descuento_tres(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             puntos += valores_letras[letra]-3
-        elif (m_fichas.descuento_dos(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        elif (m_fichas.descuento_dos(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             puntos += valores_letras[letra]-2
-        elif (m_fichas.descuento_uno(lugares_usados[posicion][0],lugares_usados[posicion][1])):
+        elif (m_fichas.descuento_uno(lugares_usados[posicion][0],lugares_usados[posicion][1],nivel)):
             puntos += valores_letras[letra]-1
         else: puntos += valores_letras[letra]
         posicion += 1
